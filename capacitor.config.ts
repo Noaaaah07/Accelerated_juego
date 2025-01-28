a4b66d3fddefedc2ac24791a9444ddf49b911bc1
Noaaaah07 <noahamengualvarela@gmail.com>
2025-01-28 08:22:08 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Accelerated_juego',
+  webDir: 'www'
+};
+
+export default config;
